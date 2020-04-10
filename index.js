9414778d602ae44ac19ac045031f1990912d87bc
Rafal Rybowski <rafalrybowski1996@gmail.com>
2020-04-11 00:08:13 +0200

    add temporary login

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser')
 
+const logingStatus = {
+	LOGED: 'loged',
+	ERROR: 'wrong login/password'
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.post('/login', function(req, res, next) {
 	var request = req.body;
-	console.log(request.username,  request.password);
-	res.json({
-		'status': "username",
-		'password': "a"
-	})
+	var login = request.login;
+	var password = request.password
+	if ( login && password) {
+		if ( password == "haslo" ){
+			res.json({
+				'status': logingStatus.LOGED,
+				'userId': 1
+			});
+		} else {
+			res.json({
+				'status': logingStatus.ERROR
+			})
+		}
+	} else {
+		res.json({
+			'status': "wrong rest request"
+		});
+	}
+	
 });
 
 app.listen(3000, () => {
